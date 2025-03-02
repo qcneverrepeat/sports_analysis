21ed25e0190770b74b7d22ad66bd6102da4d5cff
qcneverrepeat <qiancheng1948@outlook.com>
2025-03-02 23:02:11 +0800

    track player by deepsort

--- playerDetection.py
+++ playerDetection.py
@@ -1,7 +1,10 @@
-import cv2
-import torch
+import cv2, torch, joblib
 import numpy as np
+from collections import defaultdict
 from ultralytics import YOLO
+from deep_sort_realtime.deepsort_tracker import DeepSort
+from torchreid import models as reidmodels
+from torchvision import transforms
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -14,11 +17,11 @@ class playerDetection(object):
 
         self.box_color = (0, 255, 0)
         self.box_thickness = 1
-
         self.label_color = (255, 255, 255)
         self.label_bg_color = (0, 255, 0)
-
         self.player_dot_color = (255, 255, 255)
+
+        self.tracker = DeepSort(max_age = 5)
 
 
     def load_video(self, dir = "./data/video_01__momota_lindan.mp4"):
@@ -27,7 +30,13 @@ class playerDetection(object):
 
     def load_yolo_model(self, dir = "./model/yolo11n.pt", is_gpu = True):
         self.yolo_model = YOLO(dir)
-        if is_gpu: self.yolo_model.to('mps')
+        if is_gpu and torch.backends.mps.is_available(): self.yolo_model.to('mps')
+
+    def load_reid_model(self, is_gpu = True):
+        self.reid_model = reidmodels.osnet_x1_0(pretrained=True)  # 加载预训练的 OSNet 模型
+        self.reid_model.eval()  # 推理模式
+        if is_gpu and torch.backends.mps.is_available(): self.reid_model.to('mps')
+        self.reid_model_device = next(self.reid_model.parameters()).device # 模型所在的设备：cpu/mps, 后续推理向量需要据此移动设备
 
     def get_court_coords(self, type = 'diy'):
         """
@@ -40,11 +49,16 @@ class playerDetection(object):
         elif type == 'diy':
             self.court_coords = self._diy_get_court_coords()
         
-    def detect(self):
+    def detect_and_track(self):
         """
-        cv逐帧读取视频
-        yolo逐帧预测
+        cv逐帧读取视频,yolo逐帧预测,获取运动员真实坐标并绘制在虚拟球场上
+        预测完成后返回真实坐标的时间序列,用于后续数据分析
+
+        @return player_track_dict {track_id1: [(ms, track),...], track_id2: [(ms, track),...], ...}
         """
+
+        # 初始化运动员的轨迹信息
+        player_track_dict = defaultdict(list)
         
         # 创建GUI显示窗口
         cv2.namedWindow(self.wn_detect, cv2.WINDOW_NORMAL)
@@ -57,20 +71,27 @@ class playerDetection(object):
                 print("detect: 无法读取视频帧")
                 break
 
-            # 使用 YOLO 对帧进行目标检测
-            results = self.yolo_model.predict(frame, stream = False, conf = 0.5, batch = 3)
+            # 1.使用 YOLO 对帧进行目标检测
+            results = self.yolo_model.predict(frame, stream = False, conf = 0.5, batch = 3)[0]
 
-            # 对 frame 做右侧扩展，显示球场虚拟正投影
-            frame, mapping = self._expand_court_proj_2_frame(frame)
+            # 2.去掉场内运动员之外的检测对象
+            boxes_filter = self._filter_boxes(results.boxes)
 
-            # 解析检测结果 results 绘制到视频 frame 中
-            frame = self._draw_detect_res(results[0], frame)
+            # 3.使用 DeepSORT 进行目标追踪, 追踪结果写入运动轨迹信息中: player_track_dict
+            tracks = self._track_boxes(boxes_filter, frame)
+            for track in tracks: player_track_dict[track.track_id].append( (self.cap.get(cv2.CAP_PROP_POS_MSEC), track) )
 
-            # 获取运动员在场地真实坐标并绘制投影：result.boxes 线性变换
-            frame = self._draw_player_position(results[0], frame, mapping)
+            # 4.检测结果绘制到视频 frame 中
+            frame = self._draw_track_res(tracks, frame)
 
-            # GUI弹窗展示帧和追踪框等
-            cv2.imshow(self.wn_detect, frame)
+            # 5.对 frame 做右侧扩展，显示球场虚拟正投影
+            expand_frame, mapping = self._expand_court_proj_2_frame(frame)
+
+            # 6.获取运动员在场地真实坐标, 并绘制投影
+            expand_frame = self._draw_player_position(boxes_filter, expand_frame, mapping)
+
+            # 7.GUI弹窗展示帧和追踪框等
+            cv2.imshow(self.wn_detect, expand_frame)
 
             # 监听按 'q' 键退出
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -79,62 +100,174 @@ class playerDetection(object):
                 self.cap.release()
                 cv2.destroyAllWindows()
 
-    def _draw_detect_res(self, result, frame):
+        return player_track_dict
+    
+    def _track_boxes(self, boxes, frame):
         """
-        解析检测结果 result
-        将追踪框等添加到视频帧 frame 中
+        使用 DeepSORT 进行目标追踪
+
+        params:
+        - boxes (list) yolo 返回帧的 result[0].boxes
+        - frame 原始帧
+
+        return:
+        - tracks (list): 每个元素为一个追踪对象/轨迹, track_id, ...
         """
-        boxes = result.boxes  
+        bbs, embeds = [], []
+
+        for box in boxes:
+            x, y = map(int, box.xyxy[0][0:2])
+            w, h = map(int, box.xywh[0][2:4])
+            bbs.append( ([x, y, w, h], box.conf.item(), int(box.cls.item())) ) # list of tuples of ( [left,top,w,h], confidence, detection_class )
+
+            obj_chip = self._chipper(frame, [x, y, w, h]) # 检测对象的图像切片
+            obj_chip_trans = self._preprocess_4_embedding(obj_chip) # 转成 reid 模型需要的tensor
+            embed = self.reid_model(obj_chip_trans.to(self.reid_model_device)) # 切片 -> 特征向量
+            embeds.append(embed.squeeze().cpu().detach().numpy()) # list of feature vector, Deepsort中除了reid部分都需要CPU运行
+
+        tracks = self.tracker.update_tracks(bbs, embeds = embeds)
+        tracks = [track for track in tracks if track.is_confirmed()] # 筛掉未确认的轨迹
+
+        return tracks
+
+    def _chipper(self, frame, box):
+        """
+        根据边界安全地框裁剪目标区域
+
+        params:
+        - frame (np.ndarray): 原始帧，形状为 (H, W, C)
+        - box (list): 边界框，[left, top, width, height]
+
+        return:
+        - obj_chip: 裁剪后的目标图像块 NumPy 数组
+        """
+
+        left, top, width, height = box
+
+        # 确保边界框在图像范围内
+        h, w, _ = frame.shape
+        left = max(0, int(left))
+        top = max(0, int(top))
+        right = min(w, int(left + width))
+        bottom = min(h, int(top + height))
+
+        # 裁剪目标区域
+        obj_chip = frame[top:bottom, left:right]
+
+        return obj_chip
+
+    def _preprocess_4_embedding(self, chips):
+        """
+        对 OpenCV 图像进行预处理，使其符合 reid model 的输入要求
+        """
+        # OpenCV 默认是 BGR 格式，需要转换为 RGB
+        image = cv2.cvtColor(chips, cv2.COLOR_BGR2RGB)
+
+        # 定义预处理步骤
+        transform = transforms.Compose([
+            transforms.ToPILImage(),               # 转换为 PIL 图像
+            transforms.Resize((256, 128)),         # 调整大小到 (256, 128)
+            transforms.ToTensor(),                 # 转换为 Tensor
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 标准化
+        ])
+
+        return transform(image).unsqueeze(0)  # 增加 batch 维度
+
+    def _draw_track_res(self, tracks, frame):
+        """
+        将追踪结果 tracks 添加到视频帧 frame 中
+        """
+        for track in tracks:
+            track_id = track.track_id # 轨迹ID
+            left, top, right, bottom = map(int, track.to_ltrb())  # 边界框
+            cv2.rectangle(frame, (left, top), (right, bottom), self.box_color, self.box_thickness) # 绘制, 会直接修改 frame
+
+        return frame
+
+    def _filter_boxes(self, boxes):
+        """
+        过滤掉除了场内运动员之外的对象
+        1.类别不是person的
+        2.置信度小于50%的
+        3.场地外的对象去掉 TBD: 是否有更合理的做法
+        """
+        boxes_filter = []
+
         for box in boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])  # 边界框左上&右下角坐标
+            x1_rc, y1_rc = self._pixel_2_real_coords(x1, y1) # 边界框中心对应的场上真实坐标
+            x2_rc, y2_rc = self._pixel_2_real_coords(x2, y2)
+            is_in_court = self._is_intersect([x1_rc, y1_rc, x2_rc, y2_rc], [0, 0, 6.1, 13.4]) # 边界框是否和场内有交集
+
             conf = box.conf.item()  # 置信度
             cls_id = int(box.cls.item())  # 类别 ID
             cls_name = self.yolo_model.names[cls_id]  # 类别名称
 
-            if cls_name == 'person':
-                # 1.展示检测框
-                cv2.rectangle(frame, (x1, y1), (x2, y2), self.box_color, self.box_thickness)
+            if cls_name == 'person' and conf > 0.5 and is_in_court: boxes_filter.append(box)
 
-                # 2.展示标签
-                # label = f"{cls_name} {conf:.2f}"
-                # (text_width, text_height), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
-                # cv2.rectangle(frame, (x1, y1 - text_height - 10), (x1 + text_width + 10, y1), self.label_bg_color, -1)  # 填充标签背景
-                # cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, self.label_color, 2)
-            
-        return frame
-                
+        return boxes_filter
 
-    def _draw_player_position(self, result, frame, mapping):
+    def _is_intersect(self, box, region):
         """
-        mapping: virtual_x_offset, virtual_y_offset, scalar (virtual_court_height / 13.4)
+        判断 box 是否与 region 存在交集
 
-        1.result.boxes 线性变换 得到球员在场上的真实坐标, 单位m, 左上角(0,0)
-        2.在 frame 中显示一个球场正投影, 实时显示球员位置
+        参数:
+            box: [x_min, y_min, x_max, y_max]，边界框的坐标。
+            region: [x_min, y_min, x_max, y_max]，指定区域的坐标。
+
+        返回:
+            bool: 如果存在交集，返回 True；否则返回 False。
+        """
+        # 解包边界框和区域的坐标
+        box_x_min, box_y_min, box_x_max, box_y_max = box
+        region_x_min, region_y_min, region_x_max, region_y_max = region
+
+        # 判断是否相交
+        if (box_x_max > region_x_min and
+            box_x_min < region_x_max and
+            box_y_max > region_y_min and
+            box_y_min < region_y_max):
+            return True
+        return False
+
+    def _pixel_2_real_coords(self, x, y):
+        """
+        像素坐标(x,y) -> 场上的真实坐标, 单位m, 左上角(0,0)
+        """
+        player_pixel_coords = np.array([[x, y]], dtype=np.float32)
+        player_real_coords = cv2.perspectiveTransform(player_pixel_coords[None, :, :], self.perspective_matrix)[0][0]
+        return player_real_coords
+
+    def _draw_player_position(self, boxes, frame, mapping):
+        """
+        @params 
+        - mapping: virtual_x_offset, virtual_y_offset, scalar (virtual_court_height / 13.4)
+
+        1.boxes 线性变换 得到球员在场上的真实坐标, 单位m, 左上角(0,0): player_real_coords
+        2.在 frame 中显示一个虚拟球场正投影, 实时显示球员位置: player_mapped_coords
+
+        @return
+        - frame: 在虚拟球场中绘制了球员位置的帧
         """
         virtual_x_offset, virtual_y_offset, scalar = mapping
 
-        boxes = result.boxes  
+        # 对每一帧, 遍历识别到的对象
         for box in boxes:
+
             x1, y1, x2, y2 = map(int, box.xyxy[0])  # 边界框左上&右下角坐标
-            cls_id = int(box.cls.item())  # 类别 ID
-            cls_name = self.yolo_model.names[cls_id]  # 类别名称
 
-            if cls_name == 'person':
+            # 1.获取球员位置
+            player_real_coords = self._pixel_2_real_coords((x1+x2)/2, y2) # 球员的真实场上坐标, 单位m, 左上角(0,0)
+            player_mapped_coords = player_real_coords * scalar + np.array([virtual_x_offset, virtual_y_offset]) # 映射球员坐标到虚拟球场
+            x, y = player_mapped_coords.astype(int)[0], player_mapped_coords.astype(int)[1]
 
-                # 1.获取球员位置
-                player_pixel_coords = np.array([[(x1+x2)/2, y2]], dtype=np.float32)
-                player_real_coords = cv2.perspectiveTransform(player_pixel_coords[None, :, :], self.perspective_matrix)[0][0] # 球员的真实场上坐标, 单位m, 左上角(0,0)
-                player_mapped_coords = player_real_coords * scalar + np.array([virtual_x_offset, virtual_y_offset]) # 映射球员坐标到虚拟球场
-                x, y = player_mapped_coords.astype(int)[0], player_mapped_coords.astype(int)[1]
-
-                # 2.绘制球员轨迹
-                if self.trail_layer is None: self.trail_layer = np.zeros_like(frame, dtype=np.float32) # 初始化轨迹涂层
-                cv2.circle(self.trail_layer, (x, y), 2, self.player_dot_color, -1) # 在轨迹图层上绘制点
-                frame = cv2.addWeighted(frame.astype(np.float32), 1.0, self.trail_layer, 1.0, 0).astype(np.uint8) # 将轨迹图层叠加到扩展帧
-                self.trail_layer *= 0.95  # 降低轨迹亮度，使其逐渐变淡
+            # 2.绘制球员轨迹
+            if self.trail_layer is None: self.trail_layer = np.zeros_like(frame, dtype=np.float32) # 初始化轨迹涂层
+            cv2.circle(self.trail_layer, (x, y), 2, self.player_dot_color, -1) # 在轨迹图层上绘制点
+            frame = cv2.addWeighted(frame.astype(np.float32), 1.0, self.trail_layer, 1.0, 0).astype(np.uint8) # 将轨迹图层叠加到扩展帧
+            self.trail_layer *= 0.95  # 降低轨迹亮度，使其逐渐变淡
 
         return frame
-
 
     def _expand_court_proj_2_frame(self, frame):
         """
@@ -143,7 +276,9 @@ class playerDetection(object):
         球场长度: 0.6*帧高度
 
         :param frame: 原始帧
-        :return: 处理后的帧; 放缩信息
+        :return: 
+        - expanded_frame 处理后的帧
+        - mapping 放缩信息
         """
         # 1.圈定虚拟球场范围
         frame_height, frame_width = frame.shape[0], frame.shape[1]
@@ -364,10 +499,11 @@ if __name__ == "__main__":
     player_detection = playerDetection()
 
     # 1.载入视频 self.cap
-    player_detection.load_video(dir = "./demo_data/video_01__momota_lindan__2_shot.mp4")
+    player_detection.load_video(dir = "./data/video_01__momota_lindan__1_shot.mp4")
 
-    # 2.载入模型 self.yolo_model
+    # 2.载入模型 self.yolo_model / self.reid_model
     player_detection.load_yolo_model(dir = "./model/yolo11n.pt", is_gpu = True)
+    player_detection.load_reid_model()
 
     # 3.获取场地四角的图像坐标
     player_detection.get_court_coords(type = 'diy')
@@ -375,5 +511,8 @@ if __name__ == "__main__":
     # 4.像素坐标->球场坐标的映射矩阵
     player_detection.get_perspective_matrix()
 
-    # 5.逐帧做yolo人物检测
-    player_detection.detect()
+    # 5.逐帧做yolo人物检测, 获取运动员轨迹
+    player_track_dict = player_detection.detect_and_track()
+
+    # joblib.dump(player_track_dict, './data/player_track_dict')
+
